72611ec3a8517417d869a7bc1c6d599aecf1e020
Matilde Enevoldsen <matilde.enevoldsen19@gmail.com>
2018-01-29 12:07:26 +0000

    Updated Search Functionality

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,8 +5,29 @@ import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 
 class SearchPage extends React.Component {
+  	state = {
+     query: "",
+     books: []
+    }
+  
     updateQuery = (target: string) => {
       
+      const books = BooksAPI.search(target);
+      
+      if (books.error) {
+       console.log(books.error); 
+      } else {
+      	this.setState(state => {
+          state.books = books.map(book => {
+            const idx = this.props.findBookInList(book.id);
+            return idx !== -1
+              ? this.props.books[idx]
+              : Object.assign(book, { shelf: "none" });
+          });
+          return state;
+        });
+        		  console.log(this.state.books);
+      }
     }
   
 	render() {
@@ -14,7 +35,9 @@ class SearchPage extends React.Component {
         	<div>
          		<div className="search-books">
             		<div className="search-books-bar">
-              			<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              			<a 
+             			className="close-search" 
+             			onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               				<div className="search-books-input-wrapper">
                 			{/*
                   			NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -24,7 +47,10 @@ class SearchPage extends React.Component {
                   			However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   			you don't find a specific author or title. Every search is limited by search terms.
                 			*/}
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+				type="text" 
+				placeholder="Search by title or author"
+				onChange={event => this.updateQuery(event.target.value)}/>
 
               </div>
             </div>
